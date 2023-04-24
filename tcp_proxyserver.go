89ae08ee58e5713ca4c1121514b8c03403c5ac81
kristianvv <kristianvaag@gmail.com>
2023-04-24 10:45:14 +0000

    help

--- tcp_proxyserver.go
+++ tcp_proxyserver.go
@@ -1,56 +1,57 @@
 package main
 
 import (
-    "sync"
-	"net"
-	"log"
-	"io"
+    "log"
+    "net"
+    "io"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	proxyServer, err := net.Listen("tcp", "127.0.0.1:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("bundet til %s", proxyServer.Addr().String())
-    wg.Add(1)
-    go func() {
-		defer wg.Done()
-		for {
-			log.Println("før proxyServer.Accept() kallet")
-			conn, err := proxyServer.Accept()
-			if err != nil {
-				return
-			}
-			go func(client net.Conn) {
-				defer client.Close()
+    proxyServer, err := net.Listen("tcp", "172.17.0.3:8081")
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("bound to %s", proxyServer.Addr().String())
 
-				server, err := net.Dial("tcp", "127.0.0.1:")
-                if err != nil {
-					log.Println(err)
-					return
-				}
-				defer server.Close()
-				err = proxy(client, server)
-				if err != nil && err != io.EOF {
-					log.Println(err)
-				}
-			}(conn)
-		}
-	}()
-	wg.Wait()
+    for {
+        conn, err := proxyServer.Accept()
+        if err != nil {
+            log.Println(err)
+            continue
+        }
+        go func(client net.Conn) {
+            defer client.Close()
+
+            server, err := net.Dial("tcp", "172.17.0.4:8080")
+            if err != nil {
+                log.Println(err)
+                return
+            }
+            defer server.Close()
+
+            err = proxy(client, server)
+            if err != nil && err != io.EOF {
+                log.Println(err)
+            }
+        }(conn)
+    }
 }
 
-func proxy(client io.Reader, server io.Writer) error {
-	clientWriter, clientIsWriter := client.(io.Writer)
-	serverReader, serverIsReader := server.(io.Reader)
+func proxy(client, server net.Conn) error {
+    go func() {
+        defer server.Close()
+        defer client.Close()
 
-	if serverIsReader && clientIsWriter {
-		go func() {
-			_, _ = io.Copy(clientWriter, serverReader)
-		}()
-	}
-	_, err := io.Copy(server, client)
-	return err
+        _, err := io.Copy(server, client)
+        if err != nil {
+            log.Println(err)
+        }
+    }()
+
+    _, err := io.Copy(client, server)
+    if err != nil {
+        log.Println(err)
+    }
+
+    return err
 }
